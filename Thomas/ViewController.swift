44bad449367e97bfd7261ec5d65d260ad14efab3
Chang Sophia <you@example.com>
2018-08-08 12:55:48 +0800

    Initial Commit

---
+++ Thomas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thomas
+//
+//  Created by Chang Sophia on 2018/8/8.
+//  Copyright © 2018年 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
